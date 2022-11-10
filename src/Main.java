f8a260635c17627e944f777952a7f1f832b68e50
Inna <serebryakovainna@mail.ru>
2022-11-10 22:03:17 +0200

    third commit 2

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        // 3 задача
+        // цельночисленный массив
         int[] threeNumbers = new int[3];
         threeNumbers[0] = 1;
         threeNumbers[1] = 2;
         threeNumbers[2] = 3;
-        for (int i = threeNumbers.length - 1; i >= 1; i--) {
-            System.out.print(threeNumbers[i] + ", ");
-        } {
-            System.out.print(threeNumbers[0]);
+        for (int i = 0; i < 3; i++) {
+            if (threeNumbers[i] % 2 != 0) {
+                System.out.print(threeNumbers[i] + 1);
+            }
+            else {
+                System.out.print(", " + threeNumbers[i] + ", ");
+            }
         }
-        System.out.print("\n");
+        // дробный массив
         double[] threeFractions = {1.57, 7.654, 9.986};
-        for (int x = threeFractions.length - 1; x > 0; x--) {
-            System.out.print(+threeFractions[x] + ", ");
-        } {
-            System.out.print(+threeFractions[0]);
-        }
-        System.out.print("\n");
+        for (int x = threeFractions.length - 1; x > 0; x--);
+        // символьный массив
         char[] world = new char[5];
         world[0] = 'H';
         world[1] = 'e';
         world[2] = 'l';
         world[3] = 'l';
         world[4] = 'o';
-        for (int i = world.length - 1; i >= 0; i--) {
-            System.out.print(world[i]);
-        }
     }
 }
