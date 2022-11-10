7363d72a9e7d5fd9236a0ecdb3d3796bf220fe9a
Inna <serebryakovainna@mail.ru>
2022-11-10 08:57:09 +0200

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // 1, 2 задачи
+        int[] threeNumbers = new int[3];
+        threeNumbers[0] = 1;
+        threeNumbers[1] = 2;
+        threeNumbers[2] = 3; {
+            System.out.print(threeNumbers[0] + ", ");
+            System.out.print(threeNumbers[1] + ", ");
+            System.out.print(threeNumbers[2]+"\n");
+        }
+        double[] threeFractions = {1.57, 7.654, 9.986}; {
+            System.out.print(threeFractions[0] + ", ");
+            System.out.print(threeFractions[1] + ", ");
+            System.out.print(threeFractions[2]+"\n");
+        }
+        char[] world = new char[5];
+        world[0] = 'H';
+        world[1] = 'e';
+        world[2] = 'l';
+        world[3] = 'l';
+        world[4] = 'o';
+        System.out.println(world);
     }
 }
