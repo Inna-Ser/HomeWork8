e318d9baaad3505ae31f16632ead12182a977741
Inna <serebryakovainna@mail.ru>
2022-11-11 00:18:52 +0200

    second commit b

--- src/Main.java
+++ src/Main.java
@@ -5,22 +5,20 @@ public class Main {
         threeNumbers[0] = 1;
         threeNumbers[1] = 2;
         threeNumbers[2] = 3;
-        int lastSymbolT = threeNumbers[threeNumbers.length - 1];
-        for (int i = 0; i < threeNumbers.length - 1; i++) {
+        for (int i = threeNumbers.length - 1; i >= 1; i--) {
             System.out.print(threeNumbers[i] + ", ");
         } {
-            System.out.print(lastSymbolT);
+            System.out.print(threeNumbers[0]);
         }
-        System.out.println(" ");
+        System.out.print("\n");
         // дробные
         double[] threeFractions = {1.57, 7.654, 9.986};
-        double lastSymbol = threeFractions[threeFractions.length - 1];
-            for (int i = 0; i < threeFractions.length - 1; i++) {
-                System.out.print(threeFractions[i] + ", ");
-            } {
-            System.out.print(lastSymbol);
+        for (int x = threeFractions.length - 1; x > 0; x--) {
+            System.out.print(+threeFractions[x] + ", ");
+        } {
+            System.out.print(+threeFractions[0]);
         }
-        System.out.println(" ");
+        System.out.print("\n");
         // символьные
         char[] world = new char[5];
         world[0] = 'H';
@@ -28,11 +26,10 @@ public class Main {
         world[2] = 'l';
         world[3] = 'l';
         world[4] = 'o';
-        char lastSymbolW = world[world.length - 1];
-        for (int i = 0; i < world.length - 1; i++) {
+        for (int i = world.length - 1; i > 0; i--) {
             System.out.print(world[i] + ", ");
-        } {
-            System.out.print(lastSymbolW);
+            } {
+            System.out.print(world[0]);
         }
     }
 }
