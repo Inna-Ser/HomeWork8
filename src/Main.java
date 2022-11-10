e3f7fb6949b5c76d934408a26d0c0a6b3a0ede9f
Inna <serebryakovainna@mail.ru>
2022-11-10 20:08:48 +0200

    third commit 1

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,18 @@ public class Main {
         threeNumbers[0] = 1;
         threeNumbers[1] = 2;
         threeNumbers[2] = 3;
-        for (int i = 2; i >= 1; i--) {
+        for (int i = threeNumbers.length - 1; i >= 1; i--) {
             System.out.print(threeNumbers[i] + ", ");
-            } {
+        } {
             System.out.print(threeNumbers[0]);
-            }
+        }
         System.out.print("\n");
         double[] threeFractions = {1.57, 7.654, 9.986};
-            for (int x = 2; x > 0; x--) {
-                System.out.print(+threeFractions[x] + ", ");
-            } {
-                System.out.print(+threeFractions[0]);
-            }
+        for (int x = threeFractions.length - 1; x > 0; x--) {
+            System.out.print(+threeFractions[x] + ", ");
+        } {
+            System.out.print(+threeFractions[0]);
+        }
         System.out.print("\n");
         char[] world = new char[5];
         world[0] = 'H';
@@ -24,7 +24,7 @@ public class Main {
         world[2] = 'l';
         world[3] = 'l';
         world[4] = 'o';
-        for (int i = 4; i >= 0; i--) {
+        for (int i = world.length - 1; i >= 0; i--) {
             System.out.print(world[i]);
         }
     }
